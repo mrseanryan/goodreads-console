55941764fcb06a0a856b931339fba181d14133bc
str <str_ie@yahoo.co.uk>
2020-09-06 19:51:31 +0200

    Get ALL the users reviews, by paging

--- grc-show-reviews.py
+++ grc-show-reviews.py
@@ -6,8 +6,7 @@ Retrieve reviews that the user has written.
 import pdb
 import requests
 import xmltodict
-
-import goodreads_api_client as gr
+import time
 
 
 def read_file(filename):
@@ -35,16 +34,6 @@ def get_oath_token_secret():
     return read_file("oath.token-secret.credentials.txt")
 
 
-# https://www.goodreads.com/api/index#reviews.list
-resp = requests.get('https://www.goodreads.com/review/list/'+get_user_id()+'.xml',
-                    params={'key': get_dev_key(), 'v': 2, 'id': get_user_id(), 'sort': 'review'})
-# TODO - request by page (1.1 second delay between requests)
-# until hit a book that has no review
-
-data_dict = xmltodict.parse(resp.content)['GoodreadsResponse']
-reviews_books = [r for r in data_dict['reviews']['review']]
-
-
 def print_review(review):
     reduced_review = {'link': review['link'], 'body': review['body'],
                       'read_count': review['read_count'],
@@ -55,6 +44,37 @@ def print_review(review):
     print(reduced_review)
 
 
-list(map(lambda x: print_review(x), reviews_books))
+def has_review_a_body(review):
+    return not has_review_no_body(review)
+
+
+def has_review_no_body(review):
+    return review['body'] is None
+
+
+has_review = True
+page = 1
+
+while has_review:
+    # https://www.goodreads.com/api/index#reviews.list
+    resp = requests.get('https://www.goodreads.com/review/list/'+get_user_id()+'.xml',
+                        params={'key': get_dev_key(), 'v': 2, 'id': get_user_id(), 'sort': 'review', 'page': page})
+
+    data_dict = xmltodict.parse(resp.content)['GoodreadsResponse']
+    book_reviews = [r for r in data_dict['reviews']['review']]
+
+    book_reviews_filtered = list(filter(has_review_a_body, book_reviews))
+
+    list(map(lambda x: print_review(x), book_reviews_filtered))
+
+    # until hit a book that has no review
+    book_reviews_no_body = list(filter(has_review_no_body, book_reviews))
+    if any(book_reviews_no_body):
+        has_review = False
+    else:
+        page = page + 1
+        # 1.1 second delay between requests, to meet quota
+        time.sleep(1.1)
+
 
 # TODO - output in CSV format
